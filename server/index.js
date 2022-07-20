eaf4c1b3d4548a425b7fda4c9478fa3b15fabd7a
Tkhan Vin <gatorfl2014@gmail.com>
2022-07-20 19:06:56 -0400

    fix path for js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.get('/css',(req,res) => {
 }); 
 
 app.get('/js',(req,res) => {
-    res.sendFile(path.join(__dirname, '../public/main.css'))
+    res.sendFile(path.join(__dirname, '../public/main.js'))
 }); 
 
 
